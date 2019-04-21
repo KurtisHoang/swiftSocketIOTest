967b0d6796ddafb4190551ef569b8b6b5f2cdf2e
Kurtis Hoang <KurtisHoang@Kurtiss-MBP.attlocal.net>
2019-04-21 00:00:07 -0700

    reverted back

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// var PORT = process.env.PORT || 5000;
-// var express = require('express');
-// var app = express();
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
 
-// app.get("/", function(req,res){
-//     res.send("Welcome to NodeJS App on Heroku!");
-// })
+app.get("/", function(req,res){
+    res.send("Welcome to NodeJS App on Heroku!");
+})
 
-// app.listen(PORT, function() {
-//     console.log('Chat server running');
-// });
+app.listen(PORT, function() {
+    console.log('Chat server running');
+});
 
-var app = require('http').createServer()
+// var app = require('http').createServer()
 
-app.listen(8900)
+// app.listen(8900)
 
 function Player(socket) {
     var self = this
